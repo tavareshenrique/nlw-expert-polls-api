83de84fd5c0bb81d1e1e3f9fb83e9107362ddd6c
Henrique Tavares <ihenrits@gmail.com>
2024-02-08 22:12:14 -0300

    feat: new routes

---
+++ src/http/routes/get-polls.ts
@@ -0,0 +1,33 @@
+import z from 'zod'
+import { prisma } from '../../lib/prisma'
+import { FastifyInstance } from 'fastify'
+
+export async function getPoll(app: FastifyInstance) {
+  app.get('/polls/:pollId', async (request, reply) => {
+    const getPollParams = z.object({
+      pollId: z.string().uuid(),
+    })
+
+    const { pollId } = getPollParams.parse(request.params)
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        id: pollId,
+      },
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true,
+          },
+        },
+      },
+    })
+
+    if (!poll) {
+      return reply.status(404).send({ error: 'Poll not found' })
+    }
+
+    return reply.send({ poll })
+  })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,31 +1,22 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
-import { PrismaClient } from '@prisma/client'
+import cookie from '@fastify/cookie'
 
-import z from 'zod';
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-polls'
+import { voteOnPoll } from './routes/vote-on-poll'
 
-const app = fastify();
+const app = fastify()
 
-const prisma = new PrismaClient();
-
-app.post('/polls', async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string().min(3),
-  });
-
-  const { title } = createPollBody.parse(request.body);
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    }
-  });
-
-  return reply.status(201).send({
-    pollId: poll.id,
-  });
+app.register(cookie, {
+  secret: 'nlw-expert',
+  hook: 'onRequest',
 })
 
+app.register(getPoll)
+app.register(createPoll)
+app.register(voteOnPoll)
+
 app.listen({ port: 3333 }).then(() => {
-  console.log('Server is running on port 3333');
+  console.log('Server is running on port 3333')
 })
